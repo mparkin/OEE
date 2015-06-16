da3998c35bc922c4e0accef1d629f4d6035ff93c
mparkin <mparkin@gmail.com>
2015-06-17 00:17:38 +0100

    added local timezone to sql quuries to make sure the returned data reflects the right date/time

--- DataAccess.R
+++ DataAccess.R
@@ -7,8 +7,8 @@ getResourcedata = function(startDate, Res, endTime)
 {
   #Create a connection to the database called "channel"
   channel <- odbcConnect("mia4","fis","fis")
-  # need to make this timezone compliant for hours addition
-  sqlcmd <- paste("SELECT Resource, E10, E58, Reason, Availability, StartTime, EndTime FROM DW.dbo.f_ResourceHistory WHERE ResourceType = '", Res , "' AND StartTime >= '", startDate,"' AND EndTime <= '", ymd_hms(endTime,tz= Sys.getenv("TZ")) + dhours(1),"'", sep = "" )
+  
+  sqlcmd <- paste("SELECT Resource, E10, E58, Reason, Availability, StartTime, EndTime FROM DW.dbo.f_ResourceHistory WHERE ResourceType = '", Res , "' AND StartTime >= '", startDate,"' AND StartTime <= '", ymd_hms(endTime,tz= Sys.getenv("TZ")) ,"'", sep = "" )
   # Query the database and put the results into the data frame "dataframe"
   df.OUT <- sqlQuery(channel, sqlcmd)
   df.OUT <- transform(df.OUT,duration = as.double(ymd_hms(EndTime)-ymd_hms(StartTime)))
@@ -21,7 +21,7 @@ getYielddata = function(startDate,endTime)
 {
   #Create a connection to the database called "channel"
   channel <- odbcConnect("mia4","fis","fis")
-  # need to make this timezone compliant for hours addition
+
   sqlcmd <- paste("select TestTime, CoaterTime,CellTested, CellPassed, Grade, VisionTested, VisionPassed, DTStamp, Coater, Weaver,Tester from DW.dbo.f_FEOLCellVision WHERE CoaterTime >= '", startDate,"' AND CoaterTime <= '",  ymd_hms(endTime,tz= Sys.timezone()) + dhours(1) ,"'", sep = "" )
  
   # Query the database and put the results into the data frame "dataframe"
